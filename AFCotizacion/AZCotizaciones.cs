88d3207dda8afd35cc95e132a496e3fc0d3e77a9
zionweb <92651245+zionweb@users.noreply.github.com>
2025-02-03 19:59:54 -0300

    carga inicial azure function cotizacion

---
+++ AFCotizacion/AZCotizaciones.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+
+public class AZCotizaciones
+{
+    private readonly HttpClient _httpClient;
+
+    public AZCotizaciones(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+
+    [Function("PostToApi")]
+    public async Task Run([TimerTrigger("0 */5 * * * *")] TimerInfo myTimer, FunctionContext context)
+    {
+        var logger = context.GetLogger("PostToApi");
+        logger.LogInformation($"Funcion ejecutada: {DateTime.UtcNow}");
+
+
+        string baseUrl = Environment.GetEnvironmentVariable("VITE_REACT_APP_API_URL");
+        string apiUrl = $"{baseUrl}cotizaciondolar/actualizar";
+        string apiKey = Environment.GetEnvironmentVariable("VITE_REACT_APP_API_KEY");
+
+
+        var requestData = new
+        {
+
+        };
+
+        var requestJson = JsonSerializer.Serialize(requestData);
+        var content = new StringContent(requestJson, Encoding.UTF8, "application/json");
+
+        // API Key a los headers
+        _httpClient.DefaultRequestHeaders.Clear();
+        _httpClient.DefaultRequestHeaders.Add("ApiKey", apiKey);
+
+        try
+        {
+            //post
+            HttpResponseMessage response = await _httpClient.PostAsync(apiUrl, content);
+            string responseContent = await response.Content.ReadAsStringAsync();
+
+            if (response.IsSuccessStatusCode)
+            {
+                logger.LogInformation($"Éxito: {responseContent}");
+            }
+            else
+            {
+                logger.LogError($"Error: {response.StatusCode} - {responseContent}");
+            }
+        }
+        catch (Exception ex)
+        {
+            logger.LogError($"Excepción: {ex.Message}");
+        }
+    }
+}
